1dfc90f14472ee7ebb81778b53bc097c03f7701a
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2025-01-28 23:02:24 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function tt(){
    let hu=document.getElementById("txxt").value;
    alert("你的身高是:"+hu+"厘米");
    }
+// 获取按钮元素
+        const button = document.getElementById('goToManPage');
+
+        // 为按钮添加点击事件监听器
+        button.addEventListener('click', function() {
+            // 跳转到 man.html 页面
+            window.location.href = 'man.html';
+        });
